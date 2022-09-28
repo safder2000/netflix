089e0fa0f24d091c9089d49be122d8bf7bfb3b30
safdu2000 <safdu2000@gmail.com>
2022-09-28 13:30:02 +0530

    search block start

---
+++ lib/application/search/search_bloc.dart
@@ -0,0 +1,64 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:netflix_app/domain/core/falilure/main_failure.dart';
+import 'package:netflix_app/domain/downloads/idownloads_repo.dart';
+import 'package:netflix_app/domain/downloads/models/downloads.dart';
+import 'package:netflix_app/domain/search/models/search_respo/search_respo.dart';
+import 'package:netflix_app/domain/search/search_service.dart';
+
+part 'search_event.dart';
+part 'search_state.dart';
+part 'search_bloc.freezed.dart';
+
+@injectable
+class SearchBloc extends Bloc<SearchEvent, SearchState> {
+  final IDownloadRepo _downloadsService;
+  final SearchService _searchService;
+  SearchBloc(this._searchService, this._downloadsService)
+      : super(SearchState.initial()) {
+    //idle state
+    on<Initialize>((event, emit) async {
+      if (state.idleList.isNotEmpty) {
+        emit(state);
+        return;
+      }
+      emit(const SearchState(
+        searchResultList: [],
+        idleList: [],
+        isLoading: true,
+        isError: false,
+      ));
+      //get trending
+      final _result = await _downloadsService.getDownloadsImages();
+      final _state = _result.fold(
+        (MainFailure f) {
+          return const SearchState(
+            searchResultList: [],
+            idleList: [],
+            isLoading: false,
+            isError: true,
+          );
+        },
+        (List<Downloads> list) {
+          return SearchState(
+            searchResultList: [],
+            idleList: list,
+            isLoading: false,
+            isError: false,
+          );
+        },
+      );
+      emit(_state);
+
+      //show to Ui
+    });
+
+    //search result state
+    on<SearchMovie>((event, emit) {
+      // call search  movie api
+      _searchService.searchMovies(movieQuery: event.movieQuery);
+      // show to ui
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:netflix_app/application/downloads/downloads_bloc.dart';
+import 'package:netflix_app/application/search/search_bloc.dart';
 import 'package:netflix_app/domain/core/di/injectable.dart';
 import 'package:netflix_app/presentation/main_page/screen_main.dart';
 
@@ -18,7 +19,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [BlocProvider(create: (ctx) => getIt<DownloadsBloc>())],
+      providers: [
+        BlocProvider(create: (ctx) => getIt<DownloadsBloc>()),
+        BlocProvider(create: (ctx) => getIt<SearchBloc>()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'netflix',
